39f772791e6032051f028c775ac470ac572369a9
Elizabeth <ELizabeth.Do754@stu.kipptexas.org>
2023-09-29 12:10:26 -0500

    Get the Gamepad 2 Right stick to extend & lift the arm when moved up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Big_Chass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Big_Chass.java
@@ -47,36 +47,38 @@ public class Big_Chass extends LinearOpMode {
             // Put run blocks here.
             BackR.setDirection(DcMotorSimple.Direction.REVERSE);
             FrontR.setDirection(DcMotorSimple.Direction.REVERSE);
+            FrontL.setDirection(DcMotorSimple.Direction.FORWARD);
             BackL.setDirection(DcMotorSimple.Direction.REVERSE);
             UpArm2.setDirection(DcMotorSimple.Direction.REVERSE);
+
             while (opModeIsActive()) {
 
-                // Put loop blocks here.
-                // Forward/backwards = Gamepad 1's Left stick (Up and down)
-                // Strafe = Gamepad 1's Left stick (Left and right)
-                // Turn = Gamepad 1's Right Stick (Right and Left)
+                /* Put loop blocks here.
+                Forward/backwards = Gamepad 1's Left stick (Up and down)
+                Strafe = Gamepad 1's Left stick (Left and right)
+                Turn = Gamepad 1's Right Stick (Right and Left)*/
                 vertical = gamepad1.left_stick_y;
                 strafe = gamepad1.left_stick_x;
                 turn = gamepad1.right_stick_x;
 
-                // + forward/backward + strafe + turning (negative reverse wheel motion)
-                // # X ... =  set the power (below 1 makes it use less power, thus, go slower by nature)
-                BackR.setPower(0.6 * (vertical - strafe - turn));
-                FrontR.setPower(0.6 * (vertical + strafe + turn));
-                BackL.setPower(0.6 * (vertical + strafe - turn));
-                FrontL.setPower(0.6 * (vertical - strafe + turn));
+                /* + forward/backward + strafe + turning (negative reverse wheel motion)
+                # X ... =  set the power (below 1 makes it use less power, thus, go slower by nature)*/
+                BackR.setPower(0.6 * (vertical - (strafe - turn)));
+                FrontR.setPower(0.6 * (vertical + (strafe + turn)));
+                BackL.setPower(0.6 * (vertical + (strafe - turn)));
+                FrontL.setPower(0.6 * (vertical - (strafe + turn)));
                 // Lift/put down the arm = Gamepad 2's Left Stick (Up and down)
                 // Extend/shrink the arm length = Gamepad 2's Right Stick (Up and Down)
-                UpArm.setPower(gamepad2.left_stick_y);
-                UpArm2.setPower(gamepad2.left_stick_y);
-                ExpandArm.setPower(-1 * gamepad2.right_stick_y);
+                UpArm.setPower(-0.6 * gamepad2.left_stick_y);
+                UpArm2.setPower(-0.6 * gamepad2.left_stick_y);
+                ExpandArm.setPower(-0.6 * gamepad2.right_stick_y);
 
-                // Close claw = Gaming Pad 2's Left Bumper
-                // Open claw = Gaming Pad 2's Right Bumper
-                // 0-1 on a line. 0 = Left & 1 = Right (Theoretically, so use a tape marker to test the placement)
-                //A = FLick drone launcher switch
-                //B = Flick drone launch switch to original position
-                //C = activate the lever to raise the drone launcher in a 45 degree angle
+                /* Close claw = Gaming Pad 2's Left Bumper
+                Open claw = Gaming Pad 2's Right Bumper
+                0-1 on a line. 0 = Left & 1 = Right (Theoretically, so use a tape marker to test the placement)
+                A = FLick drone launcher switch
+                B = Flick drone launch switch to original position
+                C = activate the lever to raise the drone launcher in a 45 degree angle*/
                 if (gamepad2.left_bumper) {
                     ClawServo.setPosition(0.2);
                 }
