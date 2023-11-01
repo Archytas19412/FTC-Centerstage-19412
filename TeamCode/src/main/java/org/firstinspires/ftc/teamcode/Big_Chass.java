b76dd1e7126bc5e665d1a9fc07c7f75a33b3e457
Elizabeth.Do754 <elizabeth.do754@stu.kipptexas.org>
2023-11-01 16:36:19 -0500

    Set limit on Slider expansion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Big_Chass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Big_Chass.java
@@ -8,18 +8,23 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Big_Chass extends LinearOpMode {
-    //l
+    //Variables for the arm
     Servo ClawServo;
     Servo LaunchSwitch;
     Servo LaunchRaiser;
-    //l
+    DcMotor UpArm2;
+    DcMotor UpArm;
+    DcMotor ExpandArm;
+
+    //Variables of the wheels
     DcMotor BackR;
     DcMotor FrontR;
     DcMotor FrontL;
     DcMotor BackL;
-    DcMotor UpArm2;
-    DcMotor UpArm;
-    DcMotor ExpandArm;
+
+    //Variables for encoders
+    double TPR = 537.7;
+    double maximum;
 
      //This function is executed when this Op Mode is selected from the Driver Station.
     @Override
@@ -33,32 +38,32 @@ public class Big_Chass extends LinearOpMode {
         ClawServo = hardwareMap.get(Servo.class, "Claw Servo");
         LaunchSwitch = hardwareMap.get(Servo.class,"LaunchSwitch");
         LaunchRaiser = hardwareMap.get(Servo.class,"LaunchRaiser");
+        UpArm2 = hardwareMap.get(DcMotor.class, "UpArm2");
+        UpArm = hardwareMap.get(DcMotor.class, "UpArm");
+        ExpandArm = hardwareMap.get(DcMotor.class, "ExpandArm");
         BackR = hardwareMap.get(DcMotor.class, "BackR");
         FrontR = hardwareMap.get(DcMotor.class, "FrontR");
         FrontL = hardwareMap.get(DcMotor.class, "FrontL");
         BackL = hardwareMap.get(DcMotor.class, "BackL");
-        UpArm2 = hardwareMap.get(DcMotor.class, "UpArm2");
-        UpArm = hardwareMap.get(DcMotor.class, "UpArm");
-        ExpandArm = hardwareMap.get(DcMotor.class, "ExpandArm");
 
         // Put initialization blocks here.
         ClawServo.setPosition(0);
         waitForStart();
         if (opModeIsActive()) {
 
-            // Put run blocks here.
-            BackR.setDirection(DcMotorSimple.Direction.REVERSE);
-            FrontR.setDirection(DcMotorSimple.Direction.REVERSE);
-            FrontL.setDirection(DcMotorSimple.Direction.FORWARD);
-            BackL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            // Run blocks here.
             UpArm2.setDirection(DcMotorSimple.Direction.REVERSE);
             ExpandArm.setDirection(DcMotorSimple.Direction.REVERSE);
+            BackR.setDirection(DcMotorSimple.Direction.REVERSE);
+            FrontR.setDirection(DcMotorSimple.Direction.REVERSE);
+            BackL.setDirection(DcMotorSimple.Direction.REVERSE);
 
             while (opModeIsActive()) {
 
                 /* Put loop blocks here.
-                Forward/backwards = Gamepad 1's Left stick (Up and down)
-                Strafe = Gamepad 1's Left stick (Left and right)
+                Forward/backwards = Gamepad 1's Left stick (Up and Down)
+                Strafe = Gamepad 1's Left stick (Left and Right)
                 Turn = Gamepad 1's Right Stick (Right and Left)*/
                 vertical = gamepad1.left_stick_y;
                 strafe = gamepad1.left_stick_x;
@@ -70,6 +75,7 @@ public class Big_Chass extends LinearOpMode {
                 FrontR.setPower(0.6 * (vertical + (strafe + turn)));
                 BackL.setPower(0.6 * (vertical + (strafe - turn)));
                 FrontL.setPower(0.6 * (vertical - (strafe + turn)));
+
                 // Lift/put down the arm = Gamepad 2's Left Stick (Up and down)
                 // Extend/shrink the arm length = Gamepad 2's Right Stick (Up and Down)
                 UpArm.setPower(-0.8 * gamepad2.left_stick_y);
@@ -82,7 +88,6 @@ public class Big_Chass extends LinearOpMode {
                 A = FLick drone launcher switch
                 B = Flick drone launch switch to original position
                 C = activate the lever to raise the drone launcher in a 45 degree angle*/
-
                 if (gamepad2.left_bumper) {
                     ClawServo.setPosition(0.2);
                 }
@@ -95,8 +100,25 @@ public class Big_Chass extends LinearOpMode {
                 if(gamepad2.b){
                     LaunchRaiser.setPosition(0.55);
                 }
+
+                //The captions on the DriverHub
+                telemetry.addData("Slider's Amount of Ticks to Expand: ", ExpandArm.getCurrentPosition());
                 telemetry.update();
             }
+
+            /* Purpose: set the maximum for the motor's revolution via encoders
+            * Limit: Power is between 0-1
+             */
+            public void max (double turns, double power){
+
+            }
+
+            /*
+            */
+            public void beginning (){
+
+            }
+
         }
     }
 }
